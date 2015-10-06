2a44a45ab8eb0e5ef7fe9564915fb33d8076044c
Robert Osvath Boros <osvathrobi@yahoo.com>
2015-10-06 19:45:11 +0300

    - npm mod

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+var IbusInterface = require('./IbusInterface.js');
+var IbusDevices = require('./IbusDevices.js');
+
+module.exports = {
+	IbusInterface: IbusInterface,
+	IbusDevices: IbusDevices
+};
